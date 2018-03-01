5d42505f134a03e4ac07267cc66622d4f86826ea
Sasha Malko <sasha.malko@ocsbstudent.ca>
2018-03-01 10:46:06 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,10 @@
+-- Title: MovingImages
+-- Name: Sasha Malko
+-- Course: ICS2O/3C
+-- This program displays a beetleship moving acrpss the screen
+--as well as another object moving in a different direction.
+
+print ("***Testing beetleship!")
+
+--hide the status bar
+display.
